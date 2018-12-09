d8ad9be533577a9b669c36ff9fd9b9aa4015be37
Noah <noahnettey@gmail.com>
2018-12-09 23:56:40 +0200

    Environment variables etc.

---
+++ ChatApp/environment.js
@@ -0,0 +1,8 @@
+export const ENV = {
+  APIKEY: "AIzaSyDTV091nZo8Ml5o3t14CEwhp64uY38Vgi0",
+  AUTH_DOMAIN: "mcc-fall-2018-g13.firebaseapp.com",
+  DB_URL: "https://mcc-fall-2018-g13.firebaseio.com",
+  PID: "mcc-fall-2018-g13",
+  SBUCKET: "mcc-fall-2018-g13.appspot.com",
+  SEND_ID: "271319374434",
+};
